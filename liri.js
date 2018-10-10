efbdd17fcf304c505e912fd920a91dc34d26eab7
lblueter <logan11023@yahoo.com>
2018-10-09 20:09:03 -0400

    built cli app and log

--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
+var fs = require("fs")
 require('dotenv').config()
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var moment = require('moment');
+moment().format()
+var keys = require("./keys")
 
 var spotify = new Spotify(keys.spotify);
 
@@ -9,29 +12,87 @@ var spotify = new Spotify(keys.spotify);
 var youSay = process.argv[2]
 var input = process.argv[3]
 
-if (youSay === "concert-this") {
-  var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
-  request(queryUrl, function (error, response, body) {
-    if (error) {
-      console.log(error)
-    }
-    console.log(JSON.parse(body))
-  })
-} else if (youSay === "spotify-this-song") {
+var divider = "\n========================================\n"
 
-} else if (youSay === "movie-this") {
-  var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
-  request(queryUrl, function (error, response, body) {
-    if (error) {
-      console.log(error)
-    }
-    console.log(JSON.parse(body).Released)
-  })
-} else if (youSay === "do-what-it-says") {
-  console.log("JUST DO IT")
-} else {
-  console.log("Not a valid method.")
+var appendIt = function (a) {
+  fs.appendFile("log.txt", a + divider, function(err) {
+    if (err) throw err;
+    // console.log(showData);
+  });
 }
 
+var doIt = function () {
+  if (youSay === "concert-this") {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
+    request(queryUrl, function (error, response, body) {
+      if (error) {
+        console.log(error)
+      }
+      var newBody = JSON.parse(body)
+
+      var showData = [
+        "Name: " + newBody[0].venue.name,
+        "Location: " + newBody[0].venue.city + ', ' + newBody[0].venue.region,
+        "Date: " + moment(newBody[0].datetime).format("MM/DD/YYYY")
+      ].join("\n");
+      console.log(showData)
+      appendIt(showData)
+    })
+  } else if (youSay === "spotify-this-song") {
+    spotify.search({ type: "track", query: input, limit: 1 }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      var showData = [
+        "Artist: " + data.tracks.items[0].album.artists[0].name,
+        "Song: " + data.tracks.items[0].name,
+        "Album: " + data.tracks.items[0].album.name,
+        "URL: " + data.tracks.items[0].external_urls.spotify
+      ].join("\n");
+      console.log(showData)
+      appendIt(showData)
+    });
+  } else if (youSay === "movie-this") {
+    var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, function (error, response, body) {
+      if (error) {
+        console.log(error)
+      }
+      var newBody = JSON.parse(body)
+      
+      var showData = [
+        "Title: " + newBody.Title,
+        "Released: " + newBody.Year,
+        "IMDB Rating: " + newBody.imdbRating,
+        "Country: " + newBody.Country,
+        "Language: " + newBody.Language,
+        "Actors: " + newBody.Actors,
+        "Plot: " + newBody.Plot
+      ].join("\n");
+      console.log(showData)
+      appendIt(showData)
+    })
+  } else if (youSay === "do-what-it-says") {
+    console.log("JUST DO IT")
+    var whatItSaysArray = [];
+    fs.readFile("random.txt", "utf-8", function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(data)
+      whatItSaysArray = data.split(",");
+      console.log(whatItSaysArray)
+      youSay = whatItSaysArray[0];
+      input = whatItSaysArray[1];
+      console.log(youSay + " " + input);
+      doIt()
+    });
+  } else {
+    console.log("Not a valid method.")
+  }
+}
+
+doIt()
 
 
